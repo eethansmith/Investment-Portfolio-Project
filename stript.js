47dd79607db153ee77220c421a69e872c5ab12ed
psyes9 <ethan.a.smith@hotmail.co.uk>
2023-11-28 14:12:31 +0000

    Implementing backend data base

---
+++ stript.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Assuming your JSON data is stored locally in a file named `data.json`
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            displayData(data);
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+function displayData(data) {
+    const container = document.getElementById('portfolioContainer');
+    let table = '<table><tr><th>Date</th><th>Ticker Symbol</th><th>Transaction Type</th><th>No. of Shares</th><th>Price per Share</th><th>Total Value</th></tr>';
+
+    data.forEach(item => {
+        table += `<tr>
+                    <td>${item.Date}</td>
+                    <td>${item['Ticker Symbol']}</td>
+                    <td>${item['Transaction Type']}</td>
+                    <td>${item['No. of Shares']}</td>
+                    <td>${item['Price per Share USD']}</td>
+                    <td>${(parseFloat(item['No. of Shares']) * parseFloat(item['Price per Share USD'].replace('$', ''))).toFixed(2)}</td>
+                  </tr>`;
+    });
+
+    table += '</table>';
+    container.innerHTML = table;
+}
---
+++ style.cs
@@ -0,0 +1,34 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    text-align: center;
+}
+
+#portfolioContainer {
+    width: 80%;
+    margin: auto;
+    background: white;
+    padding: 20px;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+table, th, td {
+    border: 1px solid black;
+}
+
+th, td {
+    padding: 10px;
+    text-align: left;
+}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
