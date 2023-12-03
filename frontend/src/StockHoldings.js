b1840de1082ebd59187d79195a0b62fd23949c9c
psyes9 <ethan.a.smith@hotmail.co.uk>
2023-12-03 21:25:51 +0000

    cors api communication configuration

--- frontend/src/StockHoldings.js
+++ frontend/src/StockHoldings.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // Import images - adjust the path as needed
 import VAUG from './resources/VAUG.png';
 import AAPL from './resources/AAPL.png';
 import PLTR from './resources/PLTR.png';
 
 function StockHoldings() {
-  console.log('Rendering StockHoldings');
-  const holdings = [
-    // Update this with your actual data, images, and gain/loss info
-    { name: 'S&P 500', shares: 10, value: '$1000', gainLoss: '+5%', imageUrl: VAUG },
-    { name: 'Apple', shares: 15, value: '$1500', gainLoss: '+3%', imageUrl: AAPL },
-    { name: 'Palantir', shares: 15, value: '$1500', gainLoss: '-2%', imageUrl: PLTR },
-    // ... more stocks
-  ];
+  const [holdings, setHoldings] = useState([]);
+
+  // Function to fetch data from your API
+  const fetchStockHoldings = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/stock_holdings/');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data); // Check the structure of the fetched data
+      setHoldings(Object.values(data));
+      setHoldings(Object.values(data)); // Convert the object to an array and update state
+    } catch (error) {
+      console.error('Fetching data failed:', error);
+    }
+  };
+
+  // useEffect to call the fetch function when the component mounts
+  useEffect(() => {
+    fetchStockHoldings();
+  }, []);
+
+  // Function to get the corresponding image based on the stock name
+  const getImageUrl = (ticker) => {
+    switch (ticker) {
+      case 'VUAG.L':
+        return VAUG;
+      case 'AAPL':
+        return AAPL;
+      case 'PLTR':
+        return PLTR;
+      // Add cases for other tickers and their corresponding images
+      default:
+        return ''; // Default image or a placeholder
+    }
+  };
 
   return (
     <div className="stock-holdings">
       <h2>My Stock Holdings</h2>
       {holdings.map((stock, index) => (
         <button key={index} className="stock-button">
-          <img src={stock.imageUrl} alt={stock.name} className="stock-image" />
+          <img src={getImageUrl(stock.ticker)} alt={stock.name} className="stock-image" />
           <div className="stock-details">
             <div className="stock-name">{stock.name}</div>
-            <div>{`${stock.shares} shares`}</div>
+            <div>{`${stock.shares_held} shares`}</div>
           </div>
           <div className="stock-value-gain">
-            <div>{stock.value}</div>
-            <div className="gain-loss" style={{ color: stock.gainLoss.startsWith('-') ? 'red' : 'green' }}>
-              {stock.gainLoss}
+            <div>{`$${stock.value_held.toFixed(2)}`}</div>
+            <div className="gain-loss" style={{ color: stock.profit_loss_percentage < 0 ? 'red' : 'green' }}>
+              {`${stock.profit_loss_percentage.toFixed(2)}%`}
             </div>
           </div>
         </button>
@@ -35,4 +64,5 @@ function StockHoldings() {
     </div>
   );
 }
+
 export default StockHoldings;
