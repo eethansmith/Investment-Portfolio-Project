f9435eac140a282dc301bb67fb7dbb3ef02e916b
psyes9 <ethan.a.smith@hotmail.co.uk>
2023-12-11 09:23:14 +0000

    graphing for overall protfolio view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-// App.js or your main component file
 import React, { useState } from 'react';
 import './App.css';
 import homeImage from './resources/menu-con.jpg';
 import StockHoldings from './StockHoldings';
 import StockGraph from './StockGraphing';
 import HistoricHoldings from './HistoricHoldings';
+import GraphPortfolio from './HoldingsGraphing'; // Assuming this is your overall portfolio graph component
 
 function App() {
-  const [activeTab, setActiveTab] = useState('Overall Portfolio'); // default active tab
   const [selectedStock, setSelectedStock] = useState(null);
 
   const handleStockSelection = (stock) => {
-    setSelectedStock(stock);
+    setSelectedStock(stock); // Update the selected stock
   };
-    // Function to format the profit/loss percentage
-    const formatPercentage = (percentage) => {
-      if (percentage > 0) {
-        return `+${percentage.toFixed(2)}%`;
-      } else {
-        return `${percentage.toFixed(2)}%`; // Negative values already have a minus sign
-      }
-    };
+
+  // Function to format the profit/loss percentage
+  const formatPercentage = (percentage) => {
+    if (percentage > 0) {
+      return `+${percentage.toFixed(2)}%`;
+    } else {
+      return `${percentage.toFixed(2)}%`; // Negative values already have a minus sign
+    }
+  };
 
   return (
     <div className="App">
@@ -32,35 +32,29 @@ function App() {
         <div className="project-title">
           Investment Portfolio
         </div>
+        {/* Removing activeTab and setActiveTab related code from nav-links */}
         <div className="nav-links">
-          <button 
-            className={activeTab === 'Overall Portfolio' ? 'active' : ''} 
-            onClick={() => setActiveTab('Overall Portfolio')}
-          >
-            Overall Portfolio
-          </button>
           <button>Overall Profits</button>
           <button>Stock Breakdown</button>
           <button>News</button>
         </div>
       </nav>
       <header className="App-header">
-      <h1>{selectedStock ? `${selectedStock.name}` : 'Overall Portfolio'}</h1>
+        <h1>{selectedStock ? `${selectedStock.name}` : 'Overall Portfolio'}</h1>
         {selectedStock && (
           <div className="stock-info">
             <p>
               Current Value: ${selectedStock.value_held.toFixed(2)}
-            <span style={{ color: selectedStock.profit_loss_percentage >= 0 ? 'green' : 'red' }}>
+              <span style={{ color: selectedStock.profit_loss_percentage >= 0 ? 'green' : 'red' }}>
                 {` (${formatPercentage(selectedStock.profit_loss_percentage)})`}
               </span>
             </p>
             <p>Shares Held: {selectedStock.shares_held}</p>
-            {/* Add other details you want to display */}
-            </div>
+          </div>
         )}
       </header>
 
-      {selectedStock && <StockGraph ticker={selectedStock.ticker} />}
+      {!selectedStock ? <GraphPortfolio days={365} /> : <StockGraph ticker={selectedStock.ticker} />}
 
       <StockHoldings onStockSelect={handleStockSelection} />
       <HistoricHoldings/>
